f4a3cd71a0a56a03418d9cc7d9d17c8154a019a6
biancagothic892 <biancagothicg@gmail.com>
2020-10-16 12:11:30 -0300

    Commit dia 1 NLW#3

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/main.css'
+import Route from './route'
+
+// JSX - JavaScript XML
+function App() {
+  return (
+    <Route />
+  );
+}
+
+export default App;
